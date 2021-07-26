9ca38a7c575bd3ef6080cac1fc037d728a2460ac
Rachel Kahtan <kahtan.rachel@gmail.com>
2021-07-26 18:05:16 -0400

    cynthia yt video embed

--- client/components/Synthia.js
+++ client/components/Synthia.js
@@ -4,58 +4,37 @@ import * as Tone from 'tone';
 export default class Synthia extends React.Component {
   constructor(props) {
     super(props);
-    this.noteDown = this.noteDown.bind(this);
-  }
-  noteDown(elem, event) {
-    //const synth = new Tone.PolySynth().toDestination()
-    const note = elem.dataset.note;
-    //is the issue that ele, isn't defined? what is elem?
-    synth.triggerAttackRelease(note, '16n');
-    event.stopPropagation();
+    this.testClick = this.testClick.bind(this);
+    this.playerClick = this.playerClick.bind(this);
   }
   testClick() {
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now();
-    // trigger the attack immediately
     synth.triggerAttack('C4', now);
-    // wait one second before triggering the release
     synth.triggerRelease(now + 1);
   }
+  playerClick() {
+    const player = new Tone.GrainPlayer(
+      'https://www.youtube.com/watch?v=vHQL71zQxiU'
+    ).toDestination();
+    player.autostart = true;
+  }
   render() {
-    const synth = new Tone.PolySynth().toDestination();
-    const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
     return (
       <div>
         <button type='button' onClick={this.testClick}>
           TEST ME
         </button>
-        {notes.forEach((note) => {
-          let hasSharp = true;
-          if (note === 'E' || note === 'B') hasSharp = false;
-          if (hasSharp) {
-            return (
-              <div
-                className='note'
-                onClick={this.noteDown}
-                data-note={note + '4'}
-              >
-                <div
-                  className='sharp'
-                  onClick={this.noteDown}
-                  data-note={note + '#4'}
-                ></div>
-              </div>
-            );
-          } else {
-            return (
-              <div
-                className='note'
-                onClick={(event) => this.noteDown(event)}
-                data-note={note + '4'}
-              ></div>
-            );
-          }
-        })}
+        <button type='button' onClick={this.playerClick}>
+          PLAYER
+        </button>
+        <iframe
+          src='https://www.youtube.com/embed/vHQL71zQxiU'
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
       </div>
     );
   }
