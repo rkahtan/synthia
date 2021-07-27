c9c8be4411b0645f33a4f9e2f63e0b8b669970ff
Rachel Kahtan <kahtan.rachel@gmail.com>
2021-07-27 08:43:31 -0400

    three buttons

--- client/components/Synthia.js
+++ client/components/Synthia.js
@@ -4,30 +4,51 @@ import * as Tone from 'tone';
 export default class Synthia extends React.Component {
   constructor(props) {
     super(props);
-    this.testClick = this.testClick.bind(this);
-    this.playerClick = this.playerClick.bind(this);
+    this.callClick = this.callClick.bind(this);
+    this.beepClick = this.beepClick.bind(this);
+    this.anotherClick = this.anotherClick.bind(this)
   }
-  testClick() {
+  callClick() {
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now();
     synth.triggerAttack('C4', now);
     synth.triggerRelease(now + 1);
   }
-  playerClick() {
-    const player = new Tone.GrainPlayer(
-      'https://www.youtube.com/watch?v=vHQL71zQxiU'
-    ).toDestination();
-    player.autostart = true;
+  beepClick() {
+    const dist = new Tone.Distortion(0.8).toDestination();
+    const fm = new Tone.FMSynth().connect(dist);
+    fm.triggerAttackRelease("A1", "8n");
+    fm.triggerAttackRelease('C1', '4n')
+    
+  }
+  anotherClick() {
+    const oscillator = new Tone.Oscillator().toDestination().start();
+    const freqEnv = new Tone.FrequencyEnvelope({
+	    attack: 0.2,
+	    baseFrequency: "C2",
+	    octaves: 2
+    });
+    freqEnv.connect(oscillator.frequency);
+    freqEnv.triggerAttack();
+    const now = Tone.now();
+    freqEnv.triggerRelease(now+3);
+
+   
   }
   render() {
     return (
       <div>
-        <button type='button' onClick={this.testClick}>
-          TEST ME
-        </button>
-        <button type='button' onClick={this.playerClick}>
-          PLAYER
-        </button>
+        <div id='buttons'>
+          <button type='button' onClick={this.callClick}>
+            CALL ME
+          </button>
+          <button type='button' onClick={this.beepClick}>
+            BEEP ME
+          </button>
+          <button type='button' onClick={this.anotherClick}>
+            ANOTHER ONE
+          </button>
+        </div>
         <iframe
           src='https://www.youtube.com/embed/vHQL71zQxiU'
           title='YouTube video player'
